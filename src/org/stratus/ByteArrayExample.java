e8009e8260ebd4e539169d039f31aeb05a69b913
Randy Fischer <fischer@sacred.net>
2011-10-09 11:47:50 -0400

    small tweaks for fun

--- src/org/stratus/ByteArrayExample.java
+++ src/org/stratus/ByteArrayExample.java
@@ -2,59 +2,72 @@ package org.stratus;
  
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.*;
  
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
-
  
 public class ByteArrayExample {
     
-    // input key, value:           (don't care, hexstring)
-    // intermediate key, value:    (
+    //                 KEY              VALUE
+    //
+    // INPUT           D/C              String (SHA-256 hex)
+    // INTERMEDIATE    BytesWritable    IntWritable
+    // OUTPUT          Text             IntWritable
 
 
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, BytesWritable, IntWritable> {
 
-	private final static IntWritable one = new IntWritable(1);  // anything for value
- 
-	public void map(LongWritable key, Text value, OutputCollector<BytesWritable, IntWritable> output, Reporter reporter) throws IOException {
+	private final static IntWritable one = new IntWritable(1);
 
-	    String hexstr = value.toString();
+	// Take a string representing a SHA-256, e..g "2c4a2c56ba6e3fd88ee27d0348f3fc5e6fab02e68d76f526c7c2d7055d7f7ea7",
+	// and return it as a BytesWritable (32 bytes, 256 bits)
+
+	private static BytesWritable textToBytesWritable(Text hextext) {
+	    String hexstr = hextext.toString();
 	    int len = hexstr.length();
-
-	    BytesWritable difficulty;
 	    	    
 	    byte[] data = new byte[len / 2];
 	    for (int i = 0; i < len; i += 2) {
 		data[i / 2] = (byte) ((Character.digit(hexstr.charAt(i), 16) << 4) + Character.digit(hexstr.charAt(i+1), 16));
 	    }
+	    return new BytesWritable(data);
+	}
 
-	    difficulty = new BytesWritable(data);
-	    output.collect(difficulty, one);           // BytesWritable, IntWritable = 0
+ 
+	public void map(LongWritable key, Text value, OutputCollector<BytesWritable, IntWritable> output, Reporter reporter) throws IOException {
+	    output.collect(textToBytesWritable(value), one);           // BytesWritable, IntWritable
 	}
     }
 
 
- 
     public static class Reduce extends MapReduceBase implements Reducer<BytesWritable, IntWritable, Text, IntWritable> {
+
+	private static Pattern pattern = Pattern.compile(" "); 
+	private static Matcher matcher = pattern.matcher("");
+
 	public void reduce(BytesWritable key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 	    int sum = 0;
-
+	    
 	    while (values.hasNext()) {
 		sum += values.next().get();
 	    }
 
-	    output.collect(new Text(key.toString()), new IntWritable(sum));
+	    // We want to remove the spaces that key.toString() inserts in SHA-256 representation, e.g.
+	    // "2c 4a 2c 56 ba 6e 3f d8 8e e2 7d 03 48 f3 fc 5e 6f ab 02 e6 8d 76 f5 26 c7 c2 d7 05 5d 7f 7e a7"
+	    // now becomes the unreadble "2c4a2c56ba6e3fd88ee27d0348f3fc5e6fab02e68d76f526c7c2d7055d7f7ea7".
+
+	    matcher.reset(key.toString());
+	    output.collect(new Text(matcher.replaceAll("")), new IntWritable(sum));
 	}
     }
  
 
     public static void main(String[] args) throws Exception {
 	JobConf conf = new JobConf(ByteArrayExample.class);
-
 
 	conf.setJobName("bytearray");
 	conf.setBoolean("mapred.output.compress", false);
@@ -64,7 +77,7 @@ public class ByteArrayExample {
 	conf.setInputFormat(TextInputFormat.class);
 	conf.setOutputFormat(TextOutputFormat.class);
 
-	conf.setOutputKeyClass(BytesWritable.class);  // intermediate
+	conf.setOutputKeyClass(BytesWritable.class);  // Intermediate
 	conf.setOutputValueClass(IntWritable.class);
  
 	conf.setMapperClass(Map.class);
